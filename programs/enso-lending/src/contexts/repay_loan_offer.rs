df55474fcfe6b0ec8c70e75398c938e6cf3adb93
kiemtran.dev <kiemtran.dev@gmail.com>
2024-03-13 22:28:01 +0700

    change the type of amount to u64

--- programs/enso-lending/src/contexts/repay_loan_offer.rs
+++ programs/enso-lending/src/contexts/repay_loan_offer.rs
@@ -65,19 +65,22 @@ pub struct RepayLoanOffer<'info> {
 
 impl<'info> RepayLoanOffer<'info> {
     pub fn repay_loan_offer(&mut self) -> Result<()> {
-      let fee_amount = (self.loan_offer.borrow_amount as f64) * self.setting_account.borrower_fee_percent;
-      let interest_amount = (self.loan_offer.borrow_amount as f64) * self.loan_offer.interest;
+      let borrower_fee_percent = self.setting_account.borrower_fee_percent as u64;
+      let fee_amount = self.loan_offer.borrow_amount * borrower_fee_percent;
 
-      let total_amount = (self.setting_account.amount as f64) + fee_amount + interest_amount;
+      let loan_interest = self.loan_offer.interest as u64;
+      let interest_amount = self.loan_offer.borrow_amount * loan_interest;
 
-      if total_amount > self.loan_ata_asset.amount as f64 {
+      let total_amount = self.setting_account.amount + fee_amount + interest_amount;
+
+      if total_amount > self.loan_ata_asset.amount {
         return Err(RepayOfferError::NotEnoughAmount.into());
       }
 
-      self.deposit(total_amount as u64)?;
+      self.deposit(total_amount)?;
       self.loan_offer.status = LoanOfferStatus::Finished;
 
-      self.emit_event_repay_loan_offer( "repay_loan_offer".to_string(), self.loan_offer.offer_id.clone(), total_amount as u64)?;
+      self.emit_event_repay_loan_offer( "repay_loan_offer".to_string(), self.loan_offer.offer_id.clone(), total_amount)?;
       
       Ok(())
     }
