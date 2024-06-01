4a253b77c265eae645836cf6501c62a815f3574c
Kiem Tran <kiemtran@Kiems-MacBook-Pro.local>
2024-06-01 12:26:57 +0700

    fix(maximum-age): handle issue feed update age exceeds the requested maximum age

--- programs/enso-lending/src/common/constant.rs
+++ programs/enso-lending/src/common/constant.rs
@@ -29,6 +29,9 @@ pub const HOT_WALLET_PUBKEY: &str = "Hot7zcvBTa3NybAnKrKtjcW1yJcoDWao39ZAoBn4mfP
 
 pub const SOL_USD_PRICE_FEED_ID: &str = "ef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
 pub const USDC_USD_PRICE_FEED_ID: &str = "eaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a";
+#[cfg(feature = "devnet")]
+pub const MAXIMUM_AGE_PRICE_UPDATE: u64 = 100_000;
+#[cfg(not(feature = "devnet"))]
 pub const MAXIMUM_AGE_PRICE_UPDATE: u64 = 75;
 
 pub const MIN_BORROW_HEALTH_RATIO: f64 = 1.1;
